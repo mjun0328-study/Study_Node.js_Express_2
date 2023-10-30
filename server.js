dd8177da1d03bc3855926950599220449c2a6c4c
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2023-10-31 01:13:11 +0900

    웹페이지에 디자인 넣으려면

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.listen(8080, () => {
   console.log("http://localhost:8080 에서 서버 실행중");
 });
@@ -11,12 +13,4 @@ app.get("/", (요청, 응답) => {
 
 app.get("/news", (요청, 응답) => {
   응답.send("오늘 비옴");
-});
-
-app.get("/shop", (요청, 응답) => {
-  응답.send("쇼핑페이지임");
-});
-
-app.get("/about", (요청, 응답) => {
-  응답.sendFile(__dirname + "/about.html");
 });
