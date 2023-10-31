fe6c0a6bb6af55311afa8aaad9fec75074fa3a46
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2023-10-31 12:16:10 +0900

    MongoDB와 서버 연결하려면

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 서버 실행중");
-});
+const fs = require("fs");
+const { MongoClient } = require("mongodb");
+
+let db;
+const url = fs.readFileSync("secret/dbURL.txt", "utf8");
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("forum");
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 서버 실행중");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (요청, 응답) => {
   응답.sendFile(__dirname + "/index.html");
